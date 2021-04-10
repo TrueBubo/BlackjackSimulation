2a558a79488a922f76ccbd1a4f5b45a02c500b26
DevBubo <bubak.filip@gmail.com>
2021-04-10 21:50:34 +0200

    benchmarking for different limits

--- blackjackSimulation.py
+++ blackjackSimulation.py
@@ -5,7 +5,7 @@ import multiprocessing
 def newDeck():  # creates a new deck for us
     return list(range(2, 14)) * 4
 
-def playBlackJack(batchSize):
+def playBlackJack(batchSize, limit):
     playerWins = 0
     dealerWins = 0
     Draws = 0
@@ -16,7 +16,7 @@ def playBlackJack(batchSize):
             player = 0
             dealer = 0
             playerAces = 0
-            while sorted(deck)[len(deck) // 2] + player < 21:  # there are more cards, than wouldn't get us beyond our limit, than card, that would
+            while sorted(deck)[len(deck) // 2] + player < limit:  # there are more cards, than wouldn't get us beyond our limit, than card, that would
                 card = deck[-1]
                 player += card
                 if card == 11:  # player got an ace
@@ -26,7 +26,7 @@ def playBlackJack(batchSize):
                 if playerAces > 0:  # has aces to transform
                     player -= 10  # transforming an ace to 1
                     playerAces -= 1
-                    while sorted(deck)[len(deck) // 2] + player < 21 and playerAces > 0: # there are more cards, than wouldn't get us beyond our limit, than card, that would, and player can still trasform their aces
+                    while sorted(deck)[len(deck) // 2] + player < limit and playerAces > 0: # there are more cards, than wouldn't get us beyond our limit, than card, that would, and player can still trasform their aces
                         card = deck[-1]
                         player += card
                         if card == 11:
@@ -55,12 +55,12 @@ def playBlackJack(batchSize):
     queue.put((playerWins, dealerWins, Draws))  # writes out our results
 
 
-def multiprocessingStarter(cores, batchSize):
+def multiprocessingStarter(cores, batchSize, limit):
     global playerWins, dealerWins, draws, queue
 
     processes = []
     for _ in range(cores):
-        process = multiprocessing.Process(target=playBlackJack, args=(batchSize,))
+        process = multiprocessing.Process(target=playBlackJack, args=(batchSize, limit))
         process.start()
         processes.append(process)
     for process in processes:
@@ -73,18 +73,21 @@ def multiprocessingStarter(cores, batchSize):
         draws += result[2]
 
 
-queue = multiprocessing.Queue()
-cores = multiprocessing.cpu_count()
-simulations = 1_000_000
-playerWins = 0
-dealerWins = 0
-draws = 0
+if __name__ == "__main__":
+    for limit in range(22):
+        queue = multiprocessing.Queue()
+        cores = multiprocessing.cpu_count()
+        simulations = 1_000_000
+        playerWins = 0
+        dealerWins = 0
+        draws = 0
 
-multiprocessingStarter(cores, simulations // cores)
+        multiprocessingStarter(cores, simulations // cores, limit)
 
-rounds = playerWins + dealerWins + draws
-print(f"""
+        rounds = playerWins + dealerWins + draws
+        print(f"""{limit}
 Player won {round(playerWins / rounds * 100, 2)}% of the time
 Dealer won {round(dealerWins / rounds * 100, 2)}% of the time
 They drew {round(draws / rounds * 100, 2)}% of the time
-""")
+        """)
+
